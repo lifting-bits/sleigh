6d6feb6772738265a5670c55d5d8e8107c7ce716
Alessandro Gario <alessandro.gario@gmail.com>
2021-10-12 18:18:02 +0200

    cmake: Port all sleigh targets

--- README.md
+++ README.md
@@ -1,18 +1,55 @@
 # SLEIGH
 
-SLEIGH is a machine language translation and disassembly engine that is leveraged as part of the Ghidra decompiler.
+SLEIGH was designed for the GHIDRA reverse engineering platform and is used to describe microprocessors with enough detail to facilitate two major components of GHIDRA, the disassembly and decompilation engines.
 
-This repository exposes a CMake build for SLEIGH so that it can be integrated into other projects.
+This repository provides a CMake build of SLEIGH so that it can be built and packaged as a standalone library.
 
-### Build
+## Dependencies
+
+| Name | Version |
+| ---- | ------- |
+| [Git](https://git-scm.com/) | Latest |
+| [CMake](https://cmake.org/) | 3.21+ |
+| [Binutils](https://www.gnu.org/software/binutils/) | Latest |
+| [Zlib](https://zlib.net/) | Latest |
+| [Iberty](https://gcc.gnu.org/onlinedocs/libiberty/) | Latest |
+| [Doxygen](https://www.doxygen.nl/) | Latest |
+| [GraphViz](https://graphviz.org/) | Latest |
+
+## Installation
 
 ```sh
-mkdir build/
-cd build/
+# Clone SLEIGH repository
+git clone https://github.com/lifting-bits/sleigh.git
+
+# Update the Ghidra submodule
+git submodule update --init --recursive
+
+# Create build directory
+mkdir build
+cd build
+
+# Configure CMake
 cmake \
-    -DCMAKE_INSTALL_PREFIX="<path where sleigh will install>" \
+    -DSLEIGH_ENABLE_INSTALL=ON \
+    -DCMAKE_INSTALL_PREFIX="<path where SLEIGH will install>" \
     -G Ninja \
-    ../
+    ..
+
+# Build SLEIGH
 cmake --build .
+
+# Install SLEIGH
 cmake --build . --target install
+```
+
+## Packaging
+
+The CMake configuration also supports building RPM or DEB packages for SLEIGH. If the `SLEIGH_ENABLE_PACKAGING` option is set, the build will create an RPM package if it finds `rpm` in the `PATH` or a DEB package if it finds `dpkg` in the `PATH`.
+```sh
+cmake \
+    -DSLEIGH_ENABLE_PACKAGING=ON \
+    -DCMAKE_INSTALL_PREFIX="<path where SLEIGH will install>"
+    -G Ninja \
+    ..
 ```
---
+++ cmake/packaging.cmake
@@ -0,0 +1,56 @@
+#
+# Copyright (c) 2021-present, Trail of Bits, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+
+set(PACKAGE_VERSION 1)
+
+string(REPLACE "." ";" SLEIGH_VERSION_COMPONENTS "${PROJECT_VERSION}")
+list(GET SLEIGH_VERSION_COMPONENTS 0 CPACK_PACKAGE_VERSION_MAJOR)
+list(GET SLEIGH_VERSION_COMPONENTS 1 CPACK_PACKAGE_VERSION_MINOR)
+list(GET SLEIGH_VERSION_COMPONENTS 2 CPACK_PACKAGE_VERSION_PATCH)
+
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "SLEIGH was designed for the GHIDRA reverse engineering platform and is used to describe microprocessors with enough detail to facilitate two major components of GHIDRA, the disassembly and decompilation engines.")
+set(CPACK_PACKAGE_NAME "sleigh")
+set(CPACK_PACKAGE_VENDOR "NSA")
+set(CPACK_PACKAGE_CONTACT "info@trailofbits.com")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/lifting-bits/sleigh")
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${PROJECT_VERSION}-${PACKAGE_VERSION}.x86_64")
+set(CPACK_PACKAGE_RELOCATABLE ON)
+
+set(CPACK_DEBIAN_PACKAGE_RELEASE "${PACKAGE_VERSION}")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY "extra")
+set(CPACK_DEBIAN_PACKAGE_SECTION "default")
+set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${CPACK_PACKAGE_HOMEPAGE_URL}")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc-dev-bin (>=2.31), binutils-dev (>=2.34), libiberty-dev (>=20200409)")
+
+set(CPACK_RPM_PACKAGE_RELEASE_DIST "${PACKAGE_VERSION}")
+set(CPACK_RPM_PACKAGE_DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
+set(CPACK_RPM_PACKAGE_GROUP "default")
+set(CPACK_RPM_PACKAGE_REQUIRES "glibc >= 2.31, binutils-devel >= 2.34")
+
+set(CPACK_GENERATOR "TGZ")
+
+unset(rpm_executable_path CACHE)
+find_program(rpm_executable_path "rpm")
+if("${rpm_executable_path}" STREQUAL "rpm_executable_path-NOTFOUND")
+  message(WARNING "sleigh: the RPM package generator requires the 'rpm' tool")
+
+else()
+  list(APPEND CPACK_GENERATOR "RPM")
+  message(STATUS "sleigh: the RPM generator has been enabled")
+endif()
+
+unset(dpkg_executable_path CACHE)
+find_program(dpkg_executable_path "dpkg")
+if("${dpkg_executable_path}" STREQUAL "dpkg_executable_path-NOTFOUND")
+  message(WARNING "sleigh: the DEB package generator requires the 'dpkg' tool")
+else()
+  list(APPEND CPACK_GENERATOR "DEB")
+  message(STATUS "sleigh: the DEB generator has been enabled")
+endif()
+
+include(CPack)
