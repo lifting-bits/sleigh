6a57c210e31e5c8a9601b801479ae0f264b123bc
Alex Cameron <asc@tetsuo.sh>
2021-11-05 13:05:14 +1100

    Fix `libsleigh` header to check install directory

--- include/libsleigh.hh
+++ include/libsleigh.hh
@@ -10,26 +10,26 @@
 #pragma GCC diagnostic ignored "-Wdeprecated-copy"
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <address.hh>
-#include <context.hh>
-#include <emulate.hh>
-#include <error.hh>
-#include <float.hh>
-#include <globalcontext.hh>
-#include <loadimage.hh>
-#include <memstate.hh>
-#include <opbehavior.hh>
-#include <opcodes.hh>
-#include <partmap.hh>
-#include <pcoderaw.hh>
-#include <semantics.hh>
-#include <sleighbase.hh>
-#include <sleigh.hh>
-#include <slghpatexpress.hh>
-#include <slghpattern.hh>
-#include <slghsymbol.hh>
-#include <space.hh>
-#include <translate.hh>
-#include <types.h>
-#include <xml.hh>
+#include "address.hh"
+#include "context.hh"
+#include "emulate.hh"
+#include "error.hh"
+#include "float.hh"
+#include "globalcontext.hh"
+#include "loadimage.hh"
+#include "memstate.hh"
+#include "opbehavior.hh"
+#include "opcodes.hh"
+#include "partmap.hh"
+#include "pcoderaw.hh"
+#include "semantics.hh"
+#include "sleigh.hh"
+#include "sleighbase.hh"
+#include "slghpatexpress.hh"
+#include "slghpattern.hh"
+#include "slghsymbol.hh"
+#include "space.hh"
+#include "translate.hh"
+#include "types.h"
+#include "xml.hh"
 #pragma GCC diagnostic pop
