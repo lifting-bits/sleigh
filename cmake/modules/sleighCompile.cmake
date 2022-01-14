d3f5a89c538314259e8b4d837bd329f79905cd70
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-01-13 23:45:58 -0500

    Create sleigh_compile CMake module
    Useful for other projects that want to compile slaspec files

---
+++ cmake/modules/sleighCompile.cmake
@@ -0,0 +1,49 @@
+cmake_minimum_required(VERSION 3.15)
+
+# Takes the following required arguments:
+#
+#   TARGET: Named target for performing sleigh compilation
+#   SLASPEC: Path to slaspec file
+#   LOG_FILE: File to write logs
+#   OUT_FILE: Output file (should be in build directory somewhere)
+function(sleigh_compile)
+  set(options)
+  set(oneValueArgs TARGET SLASPEC LOG_FILE OUT_FILE)
+  set(multiValueArgs)
+  cmake_parse_arguments(parsed
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+
+  if(parsed_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Bad arguments: ${parsed_UNPARSED_ARGUMENTS}")
+  endif()
+  if(parsed_KEYWORDS_MISSING_VALUES)
+    message(FATAL_ERROR "Missing values for: ${parsed_KEYWORDS_MISSING_VALUES}")
+  endif()
+
+
+  set(spec_file "${parsed_SLASPEC}")
+  set(spec_build_log "${parsed_LOG_FILE}")
+  get_filename_component(spec_name "${spec_file}" NAME_WE)
+  get_filename_component(spec_dir "${spec_file}" DIRECTORY)
+  get_filename_component(spec_build_log_dir "${parsed_LOG_FILE}" DIRECTORY)
+  set(spec_out "${parsed_OUT_FILE}")
+  get_filename_component(spec_out_dir "${spec_out}" DIRECTORY)
+  # Compile the sla file
+  add_custom_command(
+    OUTPUT "${spec_out}"
+    DEPENDS "${spec_file}" "${spec_out_dir}" "${spec_build_log_dir}"
+    COMMAND "$<TARGET_FILE:sleigh::sleigh_opt>" ${spec_file} "${spec_out}" > ${spec_build_log} 2>&1
+    WORKING_DIRECTORY "${spec_dir}"
+    COMMENT "sleigh: Compiling the ${spec_name} spec file (logs written in ${spec_build_log})"
+    BYPRODUCTS ${spec_build_log}
+    VERBATIM
+  )
+
+  string(REPLACE ".." "_" spec_target_name ${spec_name})
+  set(spec_target "${parsed_TARGET}")
+  add_custom_target(${spec_target} DEPENDS ${spec_out})
+endfunction()
---
+++ cmake/sleighConfig.cmake
@@ -0,0 +1,4 @@
+include("${CMAKE_CURRENT_LIST_DIR}/sleighTargets.cmake")
+
+# Helper modules exposed by default
+include("${CMAKE_CURRENT_LIST_DIR}/modules/sleighCompile.cmake")
