09c100c172095d036179869188723c1d6034fb1c
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-02-02 14:13:35 -0500

    Cleaning sleighCompile

--- cmake/modules/sleighCompile.cmake
+++ cmake/modules/sleighCompile.cmake
@@ -2,10 +2,14 @@ cmake_minimum_required(VERSION 3.15)
 
 # Takes the following required arguments:
 #
-#   TARGET: Named target for performing sleigh compilation
+#   TARGET: Named CMake target for performing sleigh compilation
 #   SLASPEC: Path to slaspec file
 #   LOG_FILE: File to write logs
-#   OUT_FILE: Output file (should be in build directory somewhere)
+#   OUT_FILE: Compiled sleigh output file (should be in build directory somewhere)
+#
+# NOTE: This doesn't track _all_ dependencies for the slaspec compilation due
+# to the ability for slaspec files to include other files. If you want to
+# rebuild the sleigh file then you must delete the OUT_FILE
 function(sleigh_compile)
   set(options)
   set(oneValueArgs TARGET SLASPEC LOG_FILE OUT_FILE)
@@ -17,6 +21,7 @@ function(sleigh_compile)
     ${ARGN}
   )
 
+  # Sanity checking for caller
   if(parsed_UNPARSED_ARGUMENTS)
     message(FATAL_ERROR "Bad arguments: ${parsed_UNPARSED_ARGUMENTS}")
   endif()
@@ -24,29 +29,30 @@ function(sleigh_compile)
     message(FATAL_ERROR "Missing values for: ${parsed_KEYWORDS_MISSING_VALUES}")
   endif()
 
-
+  # Setup variables for paths/filenames
   set(spec_file "${parsed_SLASPEC}")
-  set(spec_build_log "${parsed_LOG_FILE}")
   get_filename_component(spec_name "${spec_file}" NAME_WE)
   get_filename_component(spec_dir "${spec_file}" DIRECTORY)
-  get_filename_component(spec_build_log_dir "${parsed_LOG_FILE}" DIRECTORY)
+
+  set(spec_build_log "${parsed_LOG_FILE}")
+  get_filename_component(spec_build_log_dir "${spec_build_log}" DIRECTORY)
+
   set(spec_out "${parsed_OUT_FILE}")
   get_filename_component(spec_out_dir "${spec_out}" DIRECTORY)
 
-  # Compile the sla file
-  # NOTE: This doesn't have _all_ dependencies for the slaspec compilation due to the ability
-  #  to include other files from the spec file
+  # Custom command to compile the sla file
   add_custom_command(
     OUTPUT "${spec_out}"
     MAIN_DEPENDENCY "${spec_file}"
     COMMAND ${CMAKE_COMMAND} -E make_directory "${spec_out_dir}"
     COMMAND ${CMAKE_COMMAND} -E make_directory "${spec_build_log_dir}"
-    COMMAND "$<TARGET_FILE:sleigh::sleigh_opt>" ${spec_file} "${spec_out}" > ${spec_build_log} 2>&1
+    COMMAND "$<TARGET_FILE:sleigh::sleigh_opt>" ${spec_file} "${spec_out}" > "${spec_build_log}" 2>&1
     WORKING_DIRECTORY "${spec_dir}"
-    COMMENT "sleigh: Compiling the ${spec_name} spec file (logs written in ${spec_build_log})"
-    BYPRODUCTS ${spec_build_log}
+    COMMENT "sleigh: Compiling the '${spec_name}' spec file (logs written in '${spec_build_log}')"
+    BYPRODUCTS "${spec_build_log}"
     VERBATIM
   )
 
-  add_custom_target(${parsed_TARGET} DEPENDS ${spec_out})
+  # Custom target for others to depend on
+  add_custom_target(${parsed_TARGET} DEPENDS "${spec_out}")
 endfunction()
