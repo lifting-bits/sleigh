2b0d733616f3235be9857b4790e06a2a8af9a755
pgoodman <pgoodman@users.noreply.github.com>
2022-04-11 00:15:23 +0000

    Bump Ghidra HEAD commit 4adddf061

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -25,7 +25,7 @@ if("${sleigh_GHIDRA_RELEASE_TYPE}" STREQUAL HEAD)
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "10.2")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "7080c10dfd73f38f60dd442bbdd79e768e120430")
+  set(ghidra_head_git_tag "4adddf061c6a3a58eb1f9b90cb1b9086e8692dbb")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
