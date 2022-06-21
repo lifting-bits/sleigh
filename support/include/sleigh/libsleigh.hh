169c2dbe231cedcf0762be2680d9f1d504553ce0
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-06-21 14:17:51 -0400

    Add standalone tool building support

--- support/include/sleigh/libsleigh.hh
+++ support/include/sleigh/libsleigh.hh
@@ -13,31 +13,31 @@
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
-#include "sleigh/address.hh"
-#include "sleigh/context.hh"
-#include "sleigh/emulate.hh"
-#include "sleigh/error.hh"
-#include "sleigh/float.hh"
-#include "sleigh/globalcontext.hh"
-#include "sleigh/loadimage.hh"
-#include "sleigh/memstate.hh"
-#include "sleigh/opbehavior.hh"
-#include "sleigh/opcodes.hh"
-#include "sleigh/partmap.hh"
-#include "sleigh/pcoderaw.hh"
-#include "sleigh/semantics.hh"
-#include "sleigh/sleigh.hh"
-#include "sleigh/sleighbase.hh"
-#include "sleigh/slghpatexpress.hh"
-#include "sleigh/slghpattern.hh"
-#include "sleigh/slghsymbol.hh"
-#include "sleigh/space.hh"
-#include "sleigh/translate.hh"
-#include "sleigh/types.h"
-#include "sleigh/xml.hh"
+#include <sleigh/address.hh>
+#include <sleigh/context.hh>
+#include <sleigh/emulate.hh>
+#include <sleigh/error.hh>
+#include <sleigh/float.hh>
+#include <sleigh/globalcontext.hh>
+#include <sleigh/loadimage.hh>
+#include <sleigh/memstate.hh>
+#include <sleigh/opbehavior.hh>
+#include <sleigh/opcodes.hh>
+#include <sleigh/partmap.hh>
+#include <sleigh/pcoderaw.hh>
+#include <sleigh/semantics.hh>
+#include <sleigh/sleigh.hh>
+#include <sleigh/sleighbase.hh>
+#include <sleigh/slghpatexpress.hh>
+#include <sleigh/slghpattern.hh>
+#include <sleigh/slghsymbol.hh>
+#include <sleigh/space.hh>
+#include <sleigh/translate.hh>
+#include <sleigh/types.h>
+#include <sleigh/xml.hh>
 #ifndef _MSC_VER
 #pragma GCC diagnostic pop
 #endif
 
-#include "sleigh/Support.h"
-#include "sleigh/Version.h"
+#include <sleigh/Support.h>
+#include <sleigh/Version.h>
