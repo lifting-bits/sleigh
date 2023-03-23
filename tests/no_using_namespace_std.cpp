259bccedf02c11db75aaf5d87c510c1413091ec8
Eric Kilmer <eric.d.kilmer@gmail.com>
2023-03-22 20:52:23 -0400

    Add test for patches that remove 'using namespace std' (#155)
    If the patch is not present or wrong, this test should fail
    
    Co-authored-by: Duncan Ogilvie <mr.exodia.tpodt@gmail.com>

---
+++ tests/no_using_namespace_std.cpp
@@ -0,0 +1,13 @@
+struct vector {};
+struct set {};
+struct unordered_map {};
+struct string {};
+
+#include <sleigh/libsleigh.hh>
+
+int main() {
+  vector v;
+  set s;
+  unordered_map m;
+  string str;
+}
