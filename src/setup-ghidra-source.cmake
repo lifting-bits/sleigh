a35ef6be2e3a4a23d3127041e62cd21cce1c45c8
pgoodman <pgoodman@users.noreply.github.com>
2023-07-03 00:20:51 +0000

    Bump Ghidra HEAD commit 73e80aa1f
    Changed files:
    
    ```
    M	Ghidra/Processors/MC6800/data/languages/6x09.sinc
    M	Ghidra/Processors/x86/certification.manifest
    A	Ghidra/Processors/x86/data/languages/x86-64-compat32.pspec
    M	Ghidra/Processors/x86/data/languages/x86-64.pspec
    M	Ghidra/Processors/x86/data/languages/x86.ldefs
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -48,7 +48,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "10.4")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "5719632656e71757f507fe9ab89510e90e96a5ca")
+  set(ghidra_head_git_tag "73e80aa1f95c607fc2b3791002ff275cb48cb492")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
