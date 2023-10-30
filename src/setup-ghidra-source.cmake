676a63659a6b4cd56edad1d3fac8ae3551259e83
pgoodman <pgoodman@users.noreply.github.com>
2023-10-30 00:17:02 +0000

    Bump Ghidra HEAD commit 7c9aa58aa
    Changed files:
    
    ```
    M	Ghidra/Features/Decompiler/src/decompile/cpp/slghsymbol.cc
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -48,7 +48,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "10.5")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "4c8c6aedbd27bdd1ba9d3efbdd33e9bf22886cef")
+  set(ghidra_head_git_tag "7c9aa58aaf92c98842ade725113aa9e49a76d9f4")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
