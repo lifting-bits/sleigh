130cbbc224e7bd99d22913cf2c0874e685736ae4
pgoodman <pgoodman@users.noreply.github.com>
2023-12-25 00:17:52 +0000

    Bump Ghidra HEAD commit f7013bdfa
    Changed files:
    
    ```
    M	Ghidra/Features/Decompiler/src/decompile/cpp/Makefile
    A	Ghidra/Features/Decompiler/src/decompile/cpp/analyzesigs.cc
    A	Ghidra/Features/Decompiler/src/decompile/cpp/analyzesigs.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/architecture.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/fspec.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/grammar.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/grammar.y
    M	Ghidra/Features/Decompiler/src/decompile/cpp/modelrules.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/printc.cc
    A	Ghidra/Features/Decompiler/src/decompile/cpp/signature.cc
    A	Ghidra/Features/Decompiler/src/decompile/cpp/signature.hh
    A	Ghidra/Features/Decompiler/src/decompile/cpp/signature_ghidra.cc
    A	Ghidra/Features/Decompiler/src/decompile/cpp/signature_ghidra.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/type.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/type.hh
    M	Ghidra/Processors/Atmel/data/languages/avr8.sinc
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_31_GCC_O0_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_31_GCC_O3_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_51_GCC_O0_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_51_GCC_O3_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_6_GCC_O0_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_6_GCC_O3_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_xmega_GCC_O0_EmulatorTest.java
    M	Ghidra/Processors/Atmel/src/test.processors/java/ghidra/test/processors/AVR8_xmega_GCC_O3_EmulatorTest.java
    A	Ghidra/Processors/Loongarch/src/main/java/ghidra/app/util/bin/format/elf/relocation/Loongarch_ElfRelocationConstants.java
    A	Ghidra/Processors/Loongarch/src/main/java/ghidra/app/util/bin/format/elf/relocation/Loongarch_ElfRelocationHandler.java
    M	Ghidra/Processors/MIPS/data/languages/mips32Instructions.sinc
    M	Ghidra/Processors/PIC/data/languages/PIC24.sinc
    M	Ghidra/Processors/PowerPC/certification.manifest
    A	Ghidra/Processors/PowerPC/data/languages/SPE_FloatMulAdd.sinc
    M	Ghidra/Processors/PowerPC/data/languages/Scalar_SPFP.sinc
    M	Ghidra/Processors/PowerPC/data/languages/ppc.ldefs
    A	Ghidra/Processors/PowerPC/data/languages/ppc_32_e500mc_be.cspec
    A	Ghidra/Processors/PowerPC/data/languages/ppc_32_e500mc_be.slaspec
    A	Ghidra/Processors/PowerPC/data/languages/ppc_32_e500mc_le.cspec
    A	Ghidra/Processors/PowerPC/data/languages/ppc_32_e500mc_le.slaspec
    M	Ghidra/Processors/PowerPC/data/languages/ppc_64_isa_vle_be.slaspec
    M	Ghidra/Processors/PowerPC/data/languages/ppc_instructions.sinc
    M	Ghidra/Processors/PowerPC/data/languages/ppc_vle.sinc
    M	Ghidra/Processors/TI_MSP430/certification.manifest
    M	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430.cspec
    A	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430.dwarf
    M	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430.ldefs
    M	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430X.cspec
    A	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430X.dwarf
    M	Ghidra/Processors/x86/data/extensions/rust/unix/cc.xml
    M	Ghidra/Processors/x86/src/main/java/ghidra/app/util/bin/format/elf/relocation/X86_64_ElfRelocationContext.java
    M	Ghidra/Processors/x86/src/main/java/ghidra/app/util/bin/format/elf/relocation/X86_64_ElfRelocationHandler.java
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -46,9 +46,9 @@ set(ghidra_patches
 if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: Try to remember to look at Ghidra/application.properties
   # TODO: CMake only likes numeric characters in the version string....
-  set(ghidra_head_version "10.5")
+  set(ghidra_head_version "11.1")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "6242fda158fed6c7dbbd6928a4a74371a212c373")
+  set(ghidra_head_git_tag "f7013bdfab8eac54387a49ed290444da9af88499")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
