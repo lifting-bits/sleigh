5be138d8771602240cb101af2983cb7e902bb148
pgoodman <pgoodman@users.noreply.github.com>
2024-01-22 00:18:53 +0000

    Bump Ghidra HEAD commit 85d276bce
    Changed files:
    
    ```
    M	Ghidra/Features/Decompiler/src/decompile/cpp/condexe.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/condexe.hh
    M	Ghidra/Processors/Loongarch/data/languages/loongarch32_instructions.sinc
    M	Ghidra/Processors/Loongarch/data/languages/loongarch_main.sinc
    M	Ghidra/Processors/PowerPC/certification.manifest
    M	Ghidra/Processors/PowerPC/data/languages/4xx.sinc
    A	Ghidra/Processors/PowerPC/data/languages/mulhwInstructions.sinc
    M	Ghidra/Processors/PowerPC/data/languages/ppc.ldefs
    M	Ghidra/Processors/PowerPC/data/languages/ppc_instructions.sinc
    M	Ghidra/Processors/PowerPC/data/languages/ppc_isa.sinc
    M	Ghidra/Processors/TI_MSP430/build.gradle
    M	Ghidra/Processors/TI_MSP430/data/languages/TI430X.sinc
    M	Ghidra/Processors/TI_MSP430/data/languages/TI_MSP430.ldefs
    M	Ghidra/Processors/TI_MSP430/data/languages/ti_msp430.opinion
    A	Ghidra/Processors/TI_MSP430/src/main/java/ghidra/app/util/bin/format/elf/extend/MSP430_ElfExtension.java
    A	Ghidra/Processors/TI_MSP430/src/main/java/ghidra/app/util/bin/format/elf/relocation/MSP430X_ElfRelocationConstants.java
    A	Ghidra/Processors/TI_MSP430/src/main/java/ghidra/app/util/bin/format/elf/relocation/MSP430X_ElfRelocationHandler.java
    A	Ghidra/Processors/TI_MSP430/src/main/java/ghidra/app/util/bin/format/elf/relocation/MSP430_ElfRelocationConstants.java
    A	Ghidra/Processors/TI_MSP430/src/main/java/ghidra/app/util/bin/format/elf/relocation/MSP430_ElfRelocationHandler.java
    A	Ghidra/Processors/TI_MSP430/src/test.processors/java/ghidra/test/processors/MSP430X_O0_EmulatorTest.java
    A	Ghidra/Processors/TI_MSP430/src/test.processors/java/ghidra/test/processors/MSP430X_O3_EmulatorTest.java
    A	Ghidra/Processors/TI_MSP430/src/test.processors/java/ghidra/test/processors/MSP430_O0_EmulatorTest.java
    A	Ghidra/Processors/TI_MSP430/src/test.processors/java/ghidra/test/processors/MSP430_O3_EmulatorTest.java
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -48,7 +48,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.1")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "3410dc1ad99bcc0449a993a054a38c2c5670269f")
+  set(ghidra_head_git_tag "85d276bce0bb2055c5556825afc810d3163ed15f")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
