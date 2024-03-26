c0b6a1ad5180709c140432d89b5d3caf596161f5
Eric Kilmer <eric.d.kilmer@gmail.com>
2024-03-26 13:16:40 -0400

    Revert "Test without patches"
    This reverts commit c6f61ff636a3a6b5f28fdf7efc365a132571b1ae.

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -55,8 +55,8 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
     PATCH_COMMAND "${GIT_EXECUTABLE}" config user.name "${ghidra_patch_user}" &&
     "${GIT_EXECUTABLE}" config user.email "${ghidra_patch_email}" &&
     "${GIT_EXECUTABLE}" am --ignore-space-change --ignore-whitespace --no-gpg-sign
-    #"${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0001-Fix-UBSAN-errors-in-decompiler.patch"
-    #"${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0002-Use-stroull-instead-of-stroul-to-parse-address-offse.patch"
+    "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0001-Fix-UBSAN-errors-in-decompiler.patch"
+    "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0002-Use-stroull-instead-of-stroul-to-parse-address-offse.patch"
     "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0003-Fix-sleighexample.patch"
   )
   string(SUBSTRING "${ghidra_git_tag}" 0 7 ghidra_short_commit)
