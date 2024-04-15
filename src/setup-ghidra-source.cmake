d931ad3709947d37c9defc7832ebfe8e31a212bc
pgoodman <43664+pgoodman@users.noreply.github.com>
2024-04-15 02:34:11 +0000

    Bump Ghidra HEAD commit 6c60bd031
    Changed files:
    
    ```
    M	Ghidra/Processors/tricore/data/languages/tricore.cspec
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -48,7 +48,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.1")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "cae9190c13709fbd889edbe8f46d28c99df0a588")
+  set(ghidra_head_git_tag "6c60bd031386c967d4e21efa4dc14bc4d84613b2")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
