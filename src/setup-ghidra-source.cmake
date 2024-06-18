9650669dc3a1498f4438e6d146f5297f75a25388
Eric Kilmer <eric.d.kilmer@gmail.com>
2024-06-18 16:37:45 -0400

    Ghidra 11.1.1 (#270)

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -22,7 +22,7 @@ set_property(CACHE sleigh_RELEASE_TYPE PROPERTY STRINGS "stable" "HEAD")
 find_package(Git REQUIRED)
 
 # Ghidra pinned stable version commit
-set(ghidra_version "11.1")
+set(ghidra_version "11.1.1")
 set(ghidra_git_tag "Ghidra_${ghidra_version}_build")
 set(ghidra_shallow TRUE)
 
