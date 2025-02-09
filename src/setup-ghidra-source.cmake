91ab01ad362a243f3b8d50098cfea6e47570cf29
Eric Kilmer <eric.d.kilmer@gmail.com>
2025-02-09 16:23:36 -0500

    Bump Ghidra HEAD commit 860c75448 (#313)
    Changed files:
    
    ```
    M       Ghidra/Processors/AARCH64/data/languages/AARCH64_golang.register.info
    M       Ghidra/Processors/x86/data/languages/x86-64-golang.register.info
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -52,7 +52,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.3")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "14527b015e09172bc4a24423fe3bc11391ef6af9")
+  set(ghidra_head_git_tag "860c754484a7b162d8196f4c4ca02145384e31be")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
