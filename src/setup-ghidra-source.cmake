0c699cb2003e62057da340beff2e8139896f21c9
pgoodman <43664+pgoodman@users.noreply.github.com>
2025-03-03 00:24:13 +0000

    Bump Ghidra HEAD commit 58e19e947
    Changed files:
    
    ```
    M       Ghidra/Processors/x86/data/languages/ia.sinc
    M       Ghidra/Processors/x86/data/languages/x86.ldefs
    ```
    
    Commit details:
    
    ```
    [Commit 1/3]
    Hash: 74066cdf78e7bc3f6b03a127a1ee647a8e36c357
    Date: 2025-02-26 20:29:50 +0000
    Message: GP-5424 updated x86 version numbers
    
    Files changed:
      M     Ghidra/Processors/x86/data/languages/x86.ldefs
    
    [Commit 2/3]
    Hash: 6e8ef8deb1e98f1cabbf6cfca6a029dc30014e4d
    Date: 2024-05-29 17:33:03 +0930
    Message: x86: PUSHF/POPF address size fixes (64-bit mode)
    
    Files changed:
      M     Ghidra/Processors/x86/data/languages/ia.sinc
    
    [Commit 3/3]
    Hash: f8b0d83b1605efbe1add57dbd001d3fe1cf49897
    Date: 2025-02-25 12:19:27 -0500
    Message: GP-5191 x86 fpu ip updates
    
    Files changed:
      M     Ghidra/Processors/x86/data/languages/ia.sinc
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -52,7 +52,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.4")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "aaf910bd82d1fd91387e002502590430be2ea658")
+  set(ghidra_head_git_tag "58e19e947c0b9eb62bbcacff5a7399026a279cfe")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
