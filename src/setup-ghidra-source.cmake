5b8f908b9f7173e6b96f59d4c92d620c668b2ec0
pgoodman <43664+pgoodman@users.noreply.github.com>
2025-04-28 00:26:23 +0000

    Bump Ghidra HEAD commit 4a46edc9f
    Changed files:
    
    ```
    M	Ghidra/Features/Decompiler/src/decompile/cpp/action.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/address.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/architecture.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/block.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/blockaction.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/constseq.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/database.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/database.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/fspec.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/fspec.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/funcdata_varnode.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/heritage.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/merge.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/prettyprint.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/printc.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/rangeutil.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/stringmanage.hh
    M	Ghidra/Features/Decompiler/src/decompile/cpp/subflow.cc
    M	Ghidra/Features/Decompiler/src/decompile/cpp/type.hh
    M	Ghidra/Processors/SuperH4/data/languages/SuperH4.sinc
    ```
    
    Commit details:
    
    ```
    [Commit 1/3]
    Hash: 6730154316df215c1c4125039c083e5d02d475b8
    Date: 2025-04-21 07:54:19 -0400
    Message: GP-0: Certify
    
    Files changed:
      M	Ghidra/Features/Decompiler/src/decompile/cpp/action.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/address.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/blockaction.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/database.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/database.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/prettyprint.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/rangeutil.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/stringmanage.hh
    
    [Commit 2/3]
    Hash: 830306162955c3bffaffa5d0c65d85f0dc49cc99
    Date: 2025-04-19 18:06:41 +0200
    Message: Many typo's These were found using the command below searching for duplicated words, and manually going through the results to remove the false positives and reword the true positives. Sometimes I removed the doubled word and sometimes I replaced the duplicated word.
    Details:
    The grep command:
    grep -nIEr '\b([a-zA-Z]+)[[:space:]*]+\1\b' ./Ghidra
    
    Files changed:
      M	Ghidra/Features/Decompiler/src/decompile/cpp/action.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/address.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/architecture.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/block.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/blockaction.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/constseq.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/database.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/database.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/fspec.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/funcdata_varnode.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/heritage.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/merge.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/prettyprint.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/printc.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/rangeutil.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/stringmanage.hh
      M	Ghidra/Features/Decompiler/src/decompile/cpp/subflow.cc
      M	Ghidra/Features/Decompiler/src/decompile/cpp/type.hh
      M	Ghidra/Processors/SuperH4/data/languages/SuperH4.sinc
    
    [Commit 3/3]
    Hash: b7e91e80a2cc8e54bf54f28a8551bd8378b30d90
    Date: 2025-04-19 17:20:51 +0200
    Message: Fix various "function" typos
    
    Files changed:
      M	Ghidra/Features/Decompiler/src/decompile/cpp/fspec.hh
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -52,7 +52,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.4")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "13834fabaaff919bce053616b8e24ef8ae0a756f")
+  set(ghidra_head_git_tag "4a46edc9feac839dbc4099b7040131578d4a463c")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
