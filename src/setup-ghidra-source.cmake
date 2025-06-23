599aec78c94f6ae7ec9740e3f7e6d15f7ae8206c
pgoodman <43664+pgoodman@users.noreply.github.com>
2025-06-23 00:28:35 +0000

    Bump Ghidra HEAD commit 8c48d9f11
    Changed files:
    
    ```
    M	Ghidra/Processors/AARCH64/data/languages/AARCH64instructions.sinc
    M	Ghidra/Processors/AARCH64/data/languages/AARCH64neon.sinc
    M	Ghidra/Processors/PA-RISC/build.gradle
    M	Ghidra/Processors/RISCV/data/languages/riscv.rv32b.sinc
    M	Ghidra/Processors/x86/data/languages/avx.sinc
    M	Ghidra/Processors/x86/data/languages/avx512.sinc
    M	Ghidra/Processors/x86/data/languages/ia.sinc
    M	Ghidra/Processors/x86/data/languages/x86.ldefs
    ```
    
    Commit details:
    
    ```
    [Commit 1/6]
    Hash: 3d54c90cd604f43bf3029f27f714c37b3b3cd806
    Date: 2025-05-30 20:47:03 -0400
    Message: Fix RISC-V minu and max instructions' definitions (Closes #8215)
    
    Files changed:
      M	Ghidra/Processors/RISCV/data/languages/riscv.rv32b.sinc
    
    [Commit 2/6]
    Hash: fe4244cbc952932b0923a34318021794ea2de805
    Date: 2025-06-11 07:56:09 -0400
    Message: GP-5622: Implemented pcode for several AARCH64 Neon instructions
    
    Files changed:
      M	Ghidra/Processors/AARCH64/data/languages/AARCH64instructions.sinc
      M	Ghidra/Processors/AARCH64/data/languages/AARCH64neon.sinc
    
    [Commit 3/6]
    Hash: b0750c2783b7ecadcd2d4dd62434a2bf09c0e7b3
    Date: 2025-06-10 09:21:39 -0400
    Message: GP-5725: Corrected operands for several AVX512 instructions
    
    Files changed:
      M	Ghidra/Processors/x86/data/languages/avx.sinc
      M	Ghidra/Processors/x86/data/languages/avx512.sinc
      M	Ghidra/Processors/x86/data/languages/ia.sinc
      M	Ghidra/Processors/x86/data/languages/x86.ldefs
    
    [Commit 4/6]
    Hash: 92f25888950324a9d6e3ddcdd36299e5bd5d1fa9
    Date: 2025-06-09 15:17:05 -0400
    Message: GP-0 Corrected build.gradle for PA-RISC to allow pcode test execution
    
    Files changed:
      M	Ghidra/Processors/PA-RISC/build.gradle
    
    [Commit 5/6]
    Hash: 17d8fdfcfa2251d9e1aa386c87ae5a34b04e797e
    Date: 2025-06-09 18:48:09 -0400
    Message: GP-0 corrected PA-RISC sleigh compile option
    
    Files changed:
      M	Ghidra/Processors/PA-RISC/build.gradle
    
    [Commit 6/6]
    Hash: 2acf28604682cc72a5adb111f7acad44130f71f9
    Date: 2025-06-09 15:17:05 -0400
    Message: GP-0 Corrected build.gradle for PA-RISC to allow pcode test execution
    
    Files changed:
      M	Ghidra/Processors/PA-RISC/build.gradle
    ```

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -52,7 +52,7 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   # TODO: CMake only likes numeric characters in the version string....
   set(ghidra_head_version "11.5")
   set(ghidra_version "${ghidra_head_version}")
-  set(ghidra_head_git_tag "5b0cac73c4aa1fc0d3b6d1aeb922c088a221cfb6")
+  set(ghidra_head_git_tag "8c48d9f1168275a039d7803267399bf418d827dd")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
